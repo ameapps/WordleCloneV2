c6434899ea3c8ddf2e52d441656e27fbc8eb493d
ameapps <git.stefano@outlook.it>
2025-08-25 09:28:11 +0200

    rimosso alert

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import { WordleGame } from './components/WordleGame.js';
 
 function App() {
-  alert('ciao')
   const app = document.createElement('div');
   const game = WordleGame("HELLO"); // parola segreta
   app.appendChild(game);
